c1fadc6e5c5463e2d7bdaf2b2764d64b638ce1fb
Benjamin <bsterner6696@gmail.com>
2016-11-15 09:50:40 -0600

    added way to have drop down list of apartments for scheduling

---
+++ RentalManagement/RentalManagement/Controllers/SchedulesController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RentalManagement.Models;
+using Twilio;
+using System.Configuration;
+
+namespace RentalManagement.Controllers
+{
+    public class SchedulesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Schedules
+        public ActionResult Index()
+        {
+            var schedule = db.Schedule.Include(s => s.Apartment);
+            return View(schedule.ToList());
+        }
+
+        // GET: Schedules/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Schedule schedule = db.Schedule.Find(id);
+            if (schedule == null)
+            {
+                return HttpNotFound();
+            }
+            return View(schedule);
+        }
+
+        // GET: Schedules/Create
+        public ActionResult Create()
+        {
+            ViewBag.ApartmentId = new SelectList(db.Apartment, "Id", "Features");
+            return View();
+        }
+
+        // POST: Schedules/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,Email,Phone,ShowingDate,ShowingTime,ApartmentId")] Schedule schedule)
+        {
+            if (ModelState.IsValid)
+            {
+                Apartment apartment = db.Apartment.FirstOrDefault(x => x.Id == schedule.ApartmentId);
+                var message = $"Appointment from {schedule.FirstName} {schedule.LastName} \n ";
+                message += $" Email: {schedule.Email} Phone: {schedule.Phone} Date: {schedule.ShowingDate.ToShortDateString()} Time: {schedule.ShowingTime.ToShortTimeString()} At: {apartment.Features}";
+                var client = new TwilioRestClient(ConfigurationManager.AppSettings["TwilioAccountSid"], ConfigurationManager.AppSettings["TwilioAuthToken"]);
+                client.SendMessage(ConfigurationManager.AppSettings["TwilioPhoneNumber"], "14143368732", message);
+
+                return RedirectToAction("Index","Apartments");
+            }
+
+            ViewBag.ApartmentId = new SelectList(db.Apartment, "Id", "Features", schedule.ApartmentId);
+            return View(schedule);
+        }
+
+        // GET: Schedules/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Schedule schedule = db.Schedule.Find(id);
+            if (schedule == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ApartmentId = new SelectList(db.Apartment, "Id", "Features", schedule.ApartmentId);
+            return View(schedule);
+        }
+
+        // POST: Schedules/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,Email,Phone,ShowingDate,ShowingTime,ApartmentId")] Schedule schedule)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(schedule).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ApartmentId = new SelectList(db.Apartment, "Id", "Features", schedule.ApartmentId);
+            return View(schedule);
+        }
+
+        // GET: Schedules/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Schedule schedule = db.Schedule.Find(id);
+            if (schedule == null)
+            {
+                return HttpNotFound();
+            }
+            return View(schedule);
+        }
+
+        // POST: Schedules/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Schedule schedule = db.Schedule.Find(id);
+            db.Schedule.Remove(schedule);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
